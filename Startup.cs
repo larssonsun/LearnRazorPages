d1b2249a09bc1af0de43d2a97e6610503278faa2
磊 孙 <77540975@qq.com>
2019-02-18 15:00:03 +0800

    relearned conf site to run under https

--- Startup.cs
+++ Startup.cs
@@ -33,14 +33,14 @@ namespace myfirstrazorpages
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            //fource to use https
-            //make sure the statment "app.UseHttpsRedirection();" is disabled
-            // services.Configure<MvcOptions>(options =>
-            // {
-            //     //if we use "Permanent" prop , broswer will not receive 302
-            //     //这里有个问题，我不知道如何制定ssl的端口，他默认是https的443。于是就报404了
-            //     options.Filters.Add(new RequireHttpsAttribute { Permanent = true });
-            // });
+            // fource to use https （注意要关系浏览器的HSTS，否则无法测试成功）
+            // make sure the statment "app.UseHttpsRedirection();" is disabled
+            services.Configure<MvcOptions>(options =>
+            {
+                // // if we use "Permanent" prop , broswer will receive 301 instead of 302
+                // // 这里测试环境下有个问题，我不知道如何制定ssl的远程端口，当前ide host的环境默认是https:5001。于是301->403
+                // options.Filters.Add(new RequireHttpsAttribute { Permanent = true });
+            });
 
             // registering the new convention with RazorPagesOptions:
             services.AddMvc().AddRazorPagesOptions(options =>
@@ -74,16 +74,23 @@ namespace myfirstrazorpages
                 //or we can use. important! for seo use following:
                 app.UseStatusCodePagesWithReExecute("/Error/{0}");
                 
-                //use url rewrite ， 指定ssl的端口为5001，因为测试服务器的host默认是5001
-                var options = new RewriteOptions().AddRedirectToHttps(301, 5001);
-                app.UseRewriter(options);
+                // // 使用urlrewriting重定向到https （注意要关系浏览器的HSTS，否则无法测试成功）
+                // // make sure the statment "app.UseHttpsRedirection();" is disabled
+                // // 1、使用 AddRedirectToHttpsPermanent 永久定向到https，但是测试服务器的https远程端口是5001而不是80
+                // var options = new RewriteOptions().AddRedirectToHttpsPermanent();
+
+                // // 2、使用AddRedirectToHttps，同时指定ssl的端口为5001，因为测试服务器的host默认是5001
+                // // (测试环境下使用其他端口将无法重定向成功：“localhost 拒绝了我们的连接请求”)
+                // var options = new RewriteOptions().AddRedirectToHttps(301, 5002);
+
+                // app.UseRewriter(options);
             }
             else
             {
                 app.UseExceptionHandler("/Error");
                 app.UseHsts();
             }
-
+            // // 注释下列语句时注意要关系浏览器的HSTS，否则无法测试成功
             // app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
